0bbd825c0215e73994f3781b8bb68a17db74d663
ScarletGuo <zguo74@wisc.edu>
2019-06-27 16:59:36 -0500

    todo: scalable

--- profiler/app/od.py
+++ profiler/app/od.py
@@ -16,7 +16,7 @@ class OutlierDetector(object):
 
     __metaclass__ = ABCMeta
     
-    def __init__(self, df, gt_idx=None, method='std'):
+    def __init__(self, df, gt_idx=None, method='std', workers=4):
         self.timer = GlobalTimer()
         self.method = method
         self.df = df
@@ -24,13 +24,14 @@ class OutlierDetector(object):
         self.overall = None
         self.structured = None
         self.combined = None
+        self.workers=workers
 
     def get_neighbors(self, left):
         X = self.df[left].values.reshape(-1,len(left))
         # calculate pairwise distance for each attribute
         distances = np.zeros((X.shape[0],X.shape[0]))
         for j in range(X.shape[1]):
-            dis = sklearn.metrics.pairwise_distances(X[:,j].reshape(-1,1), metric='cityblock')
+            dis = sklearn.metrics.pairwise_distances(X[:,j].reshape(-1,1), metric='cityblock', n_jobs=self.workers)
             # normalize distance
             dis = dis / np.nanmax(dis)
             distances = (dis <= 1e-6)*1 + distances
@@ -39,15 +40,16 @@ class OutlierDetector(object):
 
     @abstractmethod
     def get_outliers(self, data):
+        # return a mask
         pass
 
     def run_attr(self, right):
         attr_outliers = self.df.index.values[self.get_outliers(self.df[right])]
         return attr_outliers
 
-    def run_all(self, parent_sets):
+    def run_all(self, parent_sets, separate=True):
         self.timer.time_start("naive")
-        self.run_overall()
+        self.run_overall(separate)
         self.timer.time_end("naive")
         self.timer.time_start("structured")
         self.run_structured(parent_sets)
@@ -55,32 +57,52 @@ class OutlierDetector(object):
         self.run_combined()
         print(self.timer.get_stat())
 
-    def run_overall(self):
-        overall = []
-        for attr in self.df:
-            overall.extend(list(self.run_attr(attr)))
+    def run_overall(self, separate=True):
+        self.timer.time_start("naive")
+        if separate:
+            overall = []
+            for attr in self.df:
+                overall.extend(list(self.run_attr(attr)))
+        else:
+            overall = self.run_attr(self.df.columns.values)
         self.overall = overall
+        self.timer.time_end("naive")
 
     def run_attr_structured(self, left, right):
         outliers = []
         if len(left) == 0:
             return outliers
         has_same_neighbors = self.get_neighbors(left)
+        #X = self.df[left].values
+        pbar= tqdm(total=100, leave=False)
+        iter = has_same_neighbors.shape[0] / 100
         for i, row in enumerate(has_same_neighbors):
+            # indicies of neighbors
             nbr = self.df.index.values[row == len(left)]
+            # has_same = np.sum([(X[:, i] <= row[i] + 1e-6) & (X[:, i] >= row[i] - 1e-6)
+            #                    for i in range(X.shape[1])], axis=0) == 2
+            # nbr = self.df.index.values[has_same]
             if len(nbr) == 0:
                 continue
-            outliers.extend(nbr[self.get_outliers(self.df[right].values[nbr])])
-            i += 1
+            if self.method != "std":
+                outliers.extend(nbr[self.get_outliers(self.df.loc[nbr, right])])
+            else:
+                outliers.extend(nbr[self.get_outliers(self.df.loc[nbr, right], m='m2')])
+            if i % iter == 0 and i != 0:
+                pbar.update(1)
+
         return outliers
 
     def run_structured(self, parent_sets):
+        self.timer.time_start("structured")
         structured = []
         for child in tqdm(parent_sets):
             structured.extend(self.run_attr_structured(parent_sets[child], child))
         unique, count = np.unique(structured, return_counts=True)
         outliers = list(unique[count > self.param['t']*self.df.shape[0]])
         self.structured = outliers
+        self.timer.time_end("structured")
+
 
     def run_combined(self, parent_sets=None):
         if self.overall is None:
@@ -139,27 +161,18 @@ class STDDetector(OutlierDetector):
         }
 
     def get_outliers(self, data, m='m1'):
-        return abs(data - np.nanmean(data)) > self.param[m] * np.nanstd(data)
+        if np.isnumber(data):
+            return abs(data - np.nanmean(data)) > self.param[m] * np.nanstd(data)
+        # else, categorical, find low frequency items
+        
 
-    def run_attr_structured(self, left, right):
-        outliers = []
-        if len(left) == 0:
-            return outliers
-        has_same_neighbors = self.get_neighbors(left)
-        for i, row in enumerate(has_same_neighbors):
-            nbr = self.df.index.values[row == len(left)]
-            if len(nbr) == 0:
-                continue
-            outliers.extend(nbr[self.get_outliers(self.df[right].values[nbr], m='m2')])
-            i += 1
-        return outliers
 
 
 class SEVERDetector(OutlierDetector):
     def __init__(self, df, gt_idx=None):
         super(SEVERDetector, self).__init__(df, gt_idx, "sever")
         self.param = {
-
+            't': 0.05,
         }
         self.overall = None
         self.structured = None
@@ -190,7 +203,8 @@ class ISFDetector(OutlierDetector):
     def __init__(self, df, gt_idx=None, **kwargs):
         super(ISFDetector, self).__init__(df, gt_idx, "isf")
         self.param = {
-            'contamination': 0.5
+            'contamination': 0.5,
+            't': 0.05,
         }
         self.overall = None
         self.structured = None
@@ -198,13 +212,20 @@ class ISFDetector(OutlierDetector):
         self.param.update(kwargs)
 
     def get_outliers(self, data):
+        mask = np.zeros((data.shape[0]))
         if not isinstance(data, np.ndarray):
             data = data.values
         if len(data.shape) == 1:
             data = data.reshape(-1, 1)
+        # remove nan:
+        row_has_nan = np.isnan(data).any(axis=1)
+        clean = data[~row_has_nan]
         model = IsolationForest(contamination=self.param['contamination'])
-        y = model.fit_predict(data)
-        return y
+        y = model.fit_predict(clean)
+        mask[~row_has_nan] = y
+        mask = mask.astype(int)
+
+        return mask == -1
 
 
 
